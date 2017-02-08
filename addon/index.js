f9871004a31d879076bc21ebff71e53b74dd9758
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-08 17:19:58 +0100

    Add dummy implementation

---
+++ addon/index.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default function(cb) {
+  return Ember.computed(cb);
+}
---
+++ tests/unit/auto-computed-test.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import computed from 'ember-auto-computed';
+
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('Unit | auto-computed', function() {
+  it('computes initial value', function() {
+    let obj = Ember.Object.extend({
+      a: 5,
+      b: 37,
+      computed: computed(function() {
+        return this.get('a') + this.get('b');
+      }),
+    }).create();
+
+    expect(obj.get('computed')).to.equal(42);
+  });
+
+  it.skip('recomputes on dependency change', function() {
+    let obj = Ember.Object.extend({
+      a: 5,
+      b: 37,
+      computed: computed(function() {
+        return this.get('a') + this.get('b');
+      }),
+    }).create();
+
+    expect(obj.get('computed')).to.equal(42);
+
+    obj.set('a', 1);
+
+    expect(obj.get('computed')).to.equal(38);
+  });
+});
