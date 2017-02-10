a1d3a9568f378f120c7ada60254c8cdb0acbe256
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-10 12:32:40 +0100

    Track CP stack

--- addon/index.js
+++ addon/index.js
@@ -1,14 +1,33 @@
 import Ember from 'ember';
 
+let propStack = [];
+
+class StackEntry {
+  constructor(obj, keyName) {
+    this.obj = obj;
+    this.keyName = keyName;
+  }
+
+  toString() {
+    return `${this.obj}.${this.keyName}`;
+  }
+}
+
 export default function(cb) {
-  return Ember.computed(cb);
+  return Ember.computed(function() {
+    let cpName = propStack.slice(-1)[0].keyName;
+    console.log('CP:', cpName);
+    return cb.call(this);
+  });
 }
 
 let originalGet = Ember.get;
 
 function newGet(obj, keyName) {
-  console.log(obj, keyName);
-  return originalGet(obj, keyName);
+  propStack.push(new StackEntry(obj, keyName));
+  let value = originalGet(obj, keyName);
+  propStack.pop();
+  return value;
 }
 
 function newBoundGet(keyName) {
