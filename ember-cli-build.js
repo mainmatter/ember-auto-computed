d360e5e08f13b398f4ac32c279e02dadf2dec38b
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-02-16 18:25:45 +0100

    enable travis (#2)
    * enable travis
    
    * include babel polyfill
    
    …so we can use Map in Phantom
    
    * remove auto-deploy from travis config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,9 @@ let EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    babel: {
+      includePolyfill: true
+    }
   });
 
   /*
